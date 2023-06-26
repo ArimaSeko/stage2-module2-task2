4de6f6032e958d4807b24a0be5b68fa40c44fdd9
karimabdrakhmanov <j3t1x0h@gmail.com>
2023-06-26 18:52:16 +0600

    2

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,24 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletRequestListener {
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        ServletContext context = sre.getServletContext();
+        LocalDateTime initTime = LocalDateTime.now(); // Get the current time
+
+        // Set the "servletTimeInit" attribute in the context
+        context.setAttribute("servletTimeInit", initTime);
+    }
 }
